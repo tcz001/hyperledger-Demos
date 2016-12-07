5bbdd44cdaad2478016c39c8ca3a143b561a1c46
Fan Jiang <fanjiang@thoughtworks.com>
2016-12-07 14:31:23 -0500

    update simple_transaction path

--- docker/fabric-peer/workspace/cc_example.sh
+++ docker/fabric-peer/workspace/cc_example.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-peer chaincode deploy -n 'mycc' -p github.com/hyperledger/fabric/examples/chaincode/go/chaincode_example02 -c '{"Function":"init", "Args": ["a","100", "b", "200"]}'
+go get github.com/tcz001/hyperledger-Demos/chaincodes/simple_transaction
+peer chaincode deploy -n 'mycc' -p github.com/tcz001/hyperledger-Demos/chaincodes/simple_transaction -c '{"Function":"init", "Args": ["a","100", "b", "200"]}'
 peer chaincode query -n 'mycc' -c '{"Function": "query", "Args": ["a"]}'
 peer chaincode invoke -n 'mycc' -c '{"Function": "invoke", "Args": ["a", "b", "10"]}'
 peer chaincode query -n 'mycc' -c '{"Function": "query", "Args": ["a"]}'
